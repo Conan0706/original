4a17996a5a08b84a54ff8a2d56e218a02d685e36
Conan <s1f101903007@iniad.org>
2021-05-24 09:35:32 +0900

    Player,床自動生成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float slidespeed = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ëOÇ…êiÇﬁ
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+
+        //åªç›ÇÃXé≤éÊìæ
+        float pos_x = transform.position.x;
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            if (pos_x < 9.0f)
+            {
+                transform.position += new Vector3(slidespeed, 0, 0) * Time.deltaTime;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(pos_x>-9.0f)
+            {
+                transform.position -= new Vector3(slidespeed, 0, 0) * Time.deltaTime;
+            }
+        }
+        
+    }
+}
